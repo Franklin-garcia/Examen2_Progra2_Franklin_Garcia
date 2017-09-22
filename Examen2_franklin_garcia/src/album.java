661840002981ca18acff318c5478f2a8ee11dd9e
franklin garcia <franklin__garcia@unitec.edu>
2017-09-22 14:07:09 -0600

    trabajndo en mortal kombat

---
+++ Examen2_franklin_garcia/src/album.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Franklin Garcia
+ */
+public class album {
+    private String nombre;
+    private String artista;
+    private ArrayList<canciones>lista_canciones=new ArrayList();
+
+    public album() {
+    }
+
+    public album(String nombre, String artista) {
+        this.nombre = nombre;
+        this.artista = artista;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public ArrayList<canciones> getLista_canciones() {
+        return lista_canciones;
+    }
+
+    public void setLista_canciones(ArrayList<canciones> lista_canciones) {
+        this.lista_canciones = lista_canciones;
+    }
+    
+}
---
+++ Examen2_franklin_garcia/src/canciones.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Franklin Garcia
+ */
+public class canciones {
+    // Las canciones poseen Nombre, artista, duración (en segundos), genero.
+    private String nombre;
+    private String artista;
+    private double duracion;
+    private String Genero;
+
+    public canciones() {
+    }
+
+    public canciones(String nombre, String artista, double duracion, String Genero) {
+        this.nombre = nombre;
+        this.artista = artista;
+        this.duracion = duracion;
+        this.Genero = Genero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public double getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(double duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getGenero() {
+        return Genero;
+    }
+
+    public void setGenero(String Genero) {
+        this.Genero = Genero;
+    }
+    
+}
